fd5d8c34ed144e7e00af34dfdec92393f422844d
jarod06 <verystrongimeanlike@gmail.com>
2023-01-29 16:14:26 +0100

    Routage 2 (bug)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './main/main.component.html',
-  styleUrls: ['./main/main.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'devoirs-angular-buffa';
